2e6fbf6a9f5aa0503a397a2cb291d1458ed624bb
Attie Heunis <attie.heunis@outlook.com>
2013-08-01 14:10:11 +0200

    Improved login script

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 function init() {
-	alert("i");
-	document.addEventListener("deviceready", deviceReady, true);
-	delete init;
+    document.addEventListener("deviceready", deviceReady, true);
+    delete init;
 }
 
 function checkPreAuth() {
-	console.log("checkPreAuth");
+    console.log("checkPreAuth");
     var form = $("#loginForm");
-    if(window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
+    if (window.localStorage["username"] != undefined && window.localStorage["password"] != undefined) {
         $("#username", form).val(window.localStorage["username"]);
         $("#password", form).val(window.localStorage["password"]);
         handleLogin();
@@ -15,56 +14,43 @@ function checkPreAuth() {
 }
 
 function handleLogin() {
-	//alert("HandleLogin");
-    var form = $("#loginForm");    
+    //alert("HandleLogin");
+    var form = $("#loginForm");
     //disable the button so we can't resubmit while we wait
-    $("#submitButton",form).attr("disabled","disabled");
+
     var u = $("#username", form).val();
     var p = $("#password", form).val();
-    if(u != '' && p!= '') {
-    	//alert("non empty");
-         $.ajax({  
-             type: "POST",  
-             //url: "http://dev.nmmu.ac.za/shaun/service/service.asmx/GetADFullName",  
-             url: "http://dev.nmmu.ac.za/shaun/service/service.asmx/AuthenticateUser",
-             
-             //data: "{'StudentNumber':'" + $('#TextboxStudentNumber').val() + "'}",  
-             //data: "{'StaffNumber':'380152'}",
-             //data: "{'ADUsername':'shaun'}",
-             //data: "{'Username':'kingkong', 'Password':'kingkong'}",
-             //data: "{'Username':" + u + ", 'Password':" + p + "}",
-             data: "{'Username':'kingkong', 'Password':'kingkong'}",
-             contentType: "application/json; charset=utf-8",  
-             dataType: "json",  
-             success: function(msg) {  
-                 AjaxSucceeded(msg);  
-             },  
-             error: AjaxFailed  
-           }); 
-        } else {
-        navigator.notification.alert("You must enter a username and password", function() {});
-        $("#submitButton").removeAttr("disabled");
+    if (u != '' && p != '') {
+        //alert("non empty");
+        $("#submitButton", form).attr("disabled", "disabled");
+
+        $.ajax({
+            type: "POST",
+            url: "http://dev.nmmu.ac.za/shaun/service/service.asmx/AuthenticateUser", // + ?Username=" + u + "&Password=" + p,
+            data: { Username: 'kingkong', Password: 'kingkong' },
+            dataType: "jsonp"
+        }).done(function (msg) {
+            alert("done:" + msg);
+        }).fail(function (msg) {
+            alert("fail:" + msg);
+        }).always(function () {
+            $("#submitButton").removeAttr("disabled");
+        });
+        return false;
+    } else {
+        navigator.notification.alert("You must enter a username and password", function () { });
     }
     return false;
-    
 }
 
-          function AjaxSucceeded(result) {  
-              alert(result); 
-          }  
-          function AjaxFailed(result) {  
-              alert(result.status + ' ' + result.statusText);  
-          }   
-
 function deviceReady() {
-	alert("dr");
-	console.log("deviceReady");
-	$("#loginForm").on("submit",handleLogin);
+    console.log("deviceReady");
+    $("#loginForm").on("submit", handleLogin);
 
-	//$("#loginPage").on("pageinit",function() {
-	//	console.log("pageinit run");
-	//	$("#loginForm").on("submit",handleLogin);
-		//checkPreAuth();
-	//});
-	//$.mobile.changePage("#loginPage");
+    //$("#loginPage").on("pageinit",function() {
+    //	console.log("pageinit run");
+    //	$("#loginForm").on("submit",handleLogin);
+    //checkPreAuth();
+    //});
+    //$.mobile.changePage("#loginPage");
 }
